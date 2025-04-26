a82e6b7fc722d7ce9405e2e7948af8fa1d5fd95e
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-04-26 16:52:42 -0400

    a

---
+++ bibliote-app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import NavSidebar from './Components/layout/NavSidebar.vue'
+</script>
+
+<template>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ bibliote-app/src/main.ts
@@ -0,0 +1,11 @@
+import './assets/sb-admin-2.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
