1aa3f3896bd9262e48022564c35e38bfd1704d10
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-04-26 18:37:38 -0400

    a

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
-
+import NavSidebar from './Components/layout/NavSidebar.vue'
 </script>
 
 <template>
 
   <RouterView />
-
-
-
 </template>
 
 <style scoped>
