842e05f793ba881e535739e724dcf66a8bc4d3fe
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-05-31 13:24:45 -0400

    up validação

--- bibliote-app/src/views/Cadastrar/Cad-Leitor.vue
+++ bibliote-app/src/views/Cadastrar/Cad-Leitor.vue
@@ -12,8 +12,8 @@
               <div id="the-basics">
                 <input class="typeahead form-control form-control-lg" type="text" placeholder="Digite o nome completo"
                   v-model="CadLivro.nome">
-                
-                <div class="text-danger" v-if="v$.CadLivro.nome.$errors.length">
+
+                <div class="text-danger" v-if="v$.CadLivro.nome.$errors.length && v$.CadLivro.nome.$dirty">
                   <p v-for="error of v$.CadLivro.nome.$errors" :key="error.$uid">
                     <small>{{ error.$message }}</small>
                   </p>
@@ -26,6 +26,13 @@
               <div id="bloodhound">
                 <input class="typeahead form-control form-control-lg" type="text" placeholder="Digite o e-mail"
                   v-model="CadLivro.email">
+
+                <div class="text-danger" v-if="v$.CadLivro.email.$errors.length && v$.CadLivro.nome.$dirty">
+                  <p v-for="error of v$.CadLivro.email.$errors" :key="error.$uid">
+                    <small>{{ error.$message }}</small>
+                  </p>
+                </div>
+
               </div>
             </div>
           </div>
@@ -33,8 +40,15 @@
             <div class="col">
               <label>CPF</label>
               <div id="bloodhound">
-                <input class="typeahead form-control form-control-lg" type="text" placeholder="CPF do leitor"
-                  v-model="CadLivro.cpf">
+                <the-mask class="typeahead form-control form-control-lg" placeholder="Digite o CPF"
+                  v-model="CadLivro.cpf" :mask="['###.###.###-##']" />
+
+                <div class="text-danger" v-if="v$.CadLivro.cpf.$errors.length">
+                  <p v-for="error of v$.CadLivro.cpf.$errors" :key="error.$uid">
+                    <small>{{ error.$message }}</small>
+                  </p>
+                </div>
+
               </div>
             </div>
             <div class="col">
@@ -42,6 +56,13 @@
               <div id="the-basics">
                 <input class="typeahead form-control form-control-lg" type="text" placeholder="Digite o Telefone"
                   v-model="CadLivro.telefone">
+
+                <div class="text-danger" v-if="v$.CadLivro.telefone.$errors.length && v$.CadLivro.nome.$dirty">
+                  <p v-for="error of v$.CadLivro.telefone.$errors" :key="error.$uid">
+                    <small>{{ error.$message }}</small>
+                  </p>
+                </div>
+
               </div>
             </div>
             <div class="col">
@@ -49,6 +70,11 @@
               <div id="the-basics">
                 <input class="typeahead form-control form-control-lg" type="date" placeholder="States of USA"
                   v-model="CadLivro.dataNasc">
+                <div class="text-danger" v-if="v$.CadLivro.dataNasc.$errors.length && v$.CadLivro.nome.$dirty">
+                  <p v-for="error of v$.CadLivro.dataNasc.$errors" :key="error.$uid">
+                    <small>{{ error.$message }}</small>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -80,6 +106,13 @@
               <label>CEP</label>
               <div id="bloodhound">
                 <input class="typeahead form-control-lg" type="text" placeholder="CEP" v-model="CadLivro.cep">
+
+                <div class="text-danger" v-if="v$.CadLivro.cep.$errors.length && v$.CadLivro.nome.$dirty">
+                  <p v-for="error of v$.CadLivro.cep.$errors" :key="error.$uid">
+                    <small>{{ error.$message }}</small>
+                  </p>
+                </div>
+
               </div>
             </div>
           </div>
@@ -88,6 +121,7 @@
             <div class="col">
               <label for="exampleFormControlSelect1">Estado</label>
               <select class="form-control form-control-lg" id="exampleFormControlSelect1" v-model="CadLivro.estado">
+                <option value="" disabled selected>Selecione seu estado...</option>
                 <option value="AC">Acre</option>
                 <option value="AL">Alagoas</option>
                 <option value="AP">Amapá</option>
@@ -140,12 +174,26 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { TheMask } from 'vue-the-mask'
 
 import { useVuelidate } from '@vuelidate/core'
 import { required, email, minLength, helpers } from '@vuelidate/validators'
 
+// Regra personalizada para data não futura
+const notFutureDate = helpers.withMessage(
+  'Data de nascimento não pode ser no futuro',
+  (value: string) => {
+    if (!value) return true
+    return new Date(value) <= new Date()
+  }
+)
+
 export default defineComponent({
   name: 'CadLeitor',
+
+  components: {
+    TheMask // registra o componente da máscara
+  },
 
   setup() {
     return { v$: useVuelidate() }
@@ -172,19 +220,15 @@ export default defineComponent({
   validations() {
     return {
       CadLivro: {
-        nome: { 
-          minLength: helpers.withMessage('O nome deve ter no mínimo 8 carácteres!', minLength(8)) , 
-          required: helpers.withMessage('Nome é obrigatorio!', 
-          required)},
-
-        email: { required, email },
-        cpf: { required },
-        telefone: { required },
-        dataNasc: { required },
+        nome: { minLength: helpers.withMessage('O nome deve ter no mínimo 8 carácteres!', minLength(8)), required: helpers.withMessage('Nome é obrigatorio!', required) },
+        email: { email: helpers.withMessage('Este e-mail não é considerado valido!', email), required: helpers.withMessage('E-mail é obrigatorio', required) },
+        cpf: { required: helpers.withMessage('CPF é obrigatório', required), minLength: helpers.withMessage('CPF deve ter pelo menos 11 dígitos', minLength(11)) },
+        telefone: { required: helpers.withMessage('Telefone é obrigatório', required), minLength: helpers.withMessage('Telefone deve ter ao menos 10 dígitos', minLength(10)) },
+        dataNasc: { required: helpers.withMessage('Data de nascimento é obrigatória', required), notFutureDate },
         Rua: { required },
         Bairro: { required },
         Numero: { required },
-        cep: { required },
+        cep: { required: helpers.withMessage('CEP é obrigatório', required), minLength: helpers.withMessage('CEP deve conter 8 números', minLength(8)) },
         estado: { required },
         cidade: { required },
       }
---
+++ bibliote-app/vue-the-mask.d.ts
@@ -0,0 +1,5 @@
+// vue-the-mask.d.ts
+declare module 'vue-the-mask' {
+  import { DefineComponent } from 'vue';
+  export const TheMask: DefineComponent;
+}
