6c86d323ef30afc20543cd212f99157e71766a97
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-06-15 13:29:40 -0400

    up

--- bibliote-app/src/views/Cadastrar/Cad-Categoria.vue
+++ bibliote-app/src/views/Cadastrar/Cad-Categoria.vue
@@ -1,52 +1,70 @@
 <template>
-  <div class="grid-margin stretch-card">
-    <div class="card">
-      <div class="card-body">
-        <form @submit.prevent="salvar">
-          <h3 class="card-title mb-4">Cadastro de Categoria</h3>
-
-          <div class="form-group mb-3">
-            <label>Nome da Categoria</label>
-            <input
-              type="text"
-              class="form-control form-control-lg"
-              placeholder="Digite o nome da categoria"
-              v-model="categoria.nome"
-            />
-            <div class="text-danger mt-1" v-if="v$.categoria.nome.$error">
-              <small>{{ v$.categoria.nome.$errors[0].$message }}</small>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col-sm-10">
+              <h3 class="m-0">Cadastro de Categoria</h3>
+            </div>
+            <div class="col-sm-2 d-grid">
+              <RouterLink class="btn btn-secondary" to="/categorias">
+                <i class="fas fa-arrow-left me-2"></i>Voltar
+              </RouterLink>
             </div>
           </div>
-
-          <div class="form-group mb-4">
-            <label>Descrição</label>
-            <textarea
-              class="form-control"
-              rows="4"
-              placeholder="Descreva a categoria"
-              v-model="categoria.descricao"
-            ></textarea>
+        </div>
+        <div class="card-body">
+          <div class="alert alert-warning" v-if="v$.$errors.length">
+            <p class="m-0 p-0" v-for="error of v$.$errors" :key="error.$uid">
+              {{ error.$property }}: {{ error.$message }}
+            </p>
           </div>
+          <form @submit.prevent="salvar">
+            <div class="mb-3">
+              <label class="form-label">Nome da Categoria</label>
+              <input
+                type="text"
+                class="form-control"
+                v-model="categoria.nome"
+              />
+              <div class="text-danger" v-if="v$.categoria.nome.$errors.length">
+                <p v-for="error of v$.categoria.nome.$errors" :key="error.$uid">
+                  <small>{{ error.$message }}</small>
+                </p>
+              </div>
+            </div>
 
-          <div class="form-group row mt-4">
-            <div class="col-auto">
-              <button type="submit" class="btn btn-success btn-lg">Salvar</button>
+            <div class="mb-3">
+              <label class="form-label">Descrição</label>
+              <textarea
+                class="form-control"
+                rows="4"
+                v-model="categoria.descricao"
+              ></textarea>
             </div>
-            <div class="col-auto">
-              <button type="button" class="btn btn-secondary btn-lg" @click="limparCampos">Limpar</button>
+
+            <div class="d-flex gap-2">
+              <button type="submit" class="btn btn-success">
+                <i class="fas fa-save me-2"></i>Salvar
+              </button>
+              <button type="button" class="btn btn-secondary gap1" @click="limparCampos">
+                Limpar
+              </button>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useVuelidate } from '@vuelidate/core';
 import { required, minLength, helpers } from '@vuelidate/validators';
+import axios from 'axios';
+import { Toast } from '@/common/toast'; 
 
 export default defineComponent({
   name: 'CadastroCategoria',
@@ -59,8 +77,8 @@ export default defineComponent({
     return {
       categoria: {
         nome: '',
-        descricao: '',
-      },
+        descricao: ''
+      }
     };
   },
 
@@ -72,7 +90,7 @@ export default defineComponent({
           minLength: helpers.withMessage('Mínimo de 3 caracteres', minLength(3))
         },
         descricao: {}
-      },
+      }
     };
   },
 
@@ -81,12 +99,30 @@ export default defineComponent({
       const valid = await this.v$.$validate();
       if (!valid) return;
 
-      const confirmado = confirm('Deseja realmente cadastrar esta categoria?');
+      const dados = {
+        ...this.categoria,
+        id: Math.random().toString(36).substring(2, 8) 
+      };
 
-      if (confirmado) {
-        console.log('Categoria cadastrada:', this.categoria);
-        alert('Categoria cadastrada com sucesso!');
-        this.limparCampos();
+      const headers = {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': '69420'
+      };
+
+      try {
+        const response = await axios.post('http://localhost:3000/categorias', dados, { headers });
+        if (response.status === 201) {
+          Toast.fire({
+            icon: 'success',
+            title: 'Categoria cadastrada com sucesso!'
+          });
+          this.$router.push('/categorias');
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: 'Erro ao cadastrar categoria'
+        });
       }
     },
 
@@ -95,6 +131,6 @@ export default defineComponent({
       this.categoria.descricao = '';
       this.v$.$reset();
     }
-  },
+  }
 });
 </script>
