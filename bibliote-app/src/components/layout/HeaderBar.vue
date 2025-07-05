a272f85ae256c39202875728aa5706c4bb8b5d2a
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2025-07-05 00:54:17 -0400

    Logo

--- bibliote-app/src/components/layout/HeaderBar.vue
+++ bibliote-app/src/components/layout/HeaderBar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar p-0 fixed-top d-flex flex-row">
         <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo-mini" href="index.html"><img src="./images/logo-mini.svg"
+            <a class="navbar-brand brand-logo-mini"  ><img src="./images/logo.svg"
                     alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
@@ -172,3 +172,10 @@ export default defineComponent({
     name: 'HeaderBar'
 })
 </script>
+
+<style>
+.logo-mini {
+  height: 40px;
+  width: auto;
+}
+</style>
